54e13d5fc1818f1aeda28ac4f477f67bef42cea0
IDAM <innes.andersonmorrison@gmail.com>
2020-09-19 23:08:21 +0100

    version bump to 0.1.11

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#![deny(clippy::all)]
 /**
  * My personal penrose config (build from the head of develop)
  */
+
 #[macro_use]
 extern crate penrose;
 
@@ -8,7 +10,7 @@ use penrose::{
     bindings::MouseEvent,
     contrib::extensions::Scratchpad,
     draw::{dwm_bar, TextStyle, XCBDraw},
-    helpers::{spawn, spawn_for_output},
+    helpers::{index_selectors, spawn, spawn_for_output},
     layout::{bottom_stack, monocle, side_stack, Layout, LayoutConf},
     Backward, Config, Forward, Less, More, Result, Selector, WindowManager, XcbConnection,
 };
@@ -16,7 +18,7 @@ use simplelog::{LevelFilter, SimpleLogger};
 use std::env;
 
 const HEIGHT: usize = 18;
-const PROFONT: &'static str = "ProFont For Powerline";
+const PROFONT: &str = "ProFont For Powerline";
 
 const BLACK: u32 = 0x282828ff;
 const GREY: u32 = 0x3c3836ff;
@@ -71,61 +73,58 @@ fn main() -> Result<()> {
     let home = env::var("HOME").unwrap();
     let script = format!("{}/bin/scripts/power-menu.sh", home);
     let power_menu = Box::new(move |wm: &mut WindowManager| {
-        match spawn_for_output(&script) {
-            Ok(o) => match o.as_str() {
-                "restart-wm\n" => wm.exit(),
-                _ => (), // other options are handled by the script
-            },
-            Err(_) => (),
+        if let Ok(o) = spawn_for_output(&script) {
+            if o.as_str() == "restart-wm\n" {
+                wm.exit();
+            }
         }
     });
 
     let key_bindings = gen_keybindings! {
         // Program launch
-        "M-semicolon" => run_external!("rofi-apps"),
-        "M-Return" => run_external!("st"),
+        "M-semicolon" => run_external!("rofi-apps");
+        "M-Return" => run_external!("st");
 
         // actions
-        "M-A-s" => run_external!("screenshot"),
-        "M-A-k" => run_external!("toggle-kb-for-tada"),
-        "M-A-l" => run_external!("lock-screen"),
-        "M-A-m" => run_external!("xrandr --output HDMI-1 --auto --right-of eDP-1 "),
-        "M-A-d" => run_internal!(detect_screens),
-        "M-slash" => sp.toggle(),
+        "M-A-s" => run_external!("screenshot");
+        "M-A-k" => run_external!("toggle-kb-for-tada");
+        "M-A-l" => run_external!("lock-screen");
+        "M-A-m" => run_external!("xrandr --output HDMI-1 --auto --right-of eDP-1 ");
+        "M-A-d" => run_internal!(detect_screens);
+        "M-slash" => sp.toggle();
 
         // client management
-        "M-j" => run_internal!(cycle_client, Forward),
-        "M-k" => run_internal!(cycle_client, Backward),
-        "M-S-j" => run_internal!(drag_client, Forward),
-        "M-S-k" => run_internal!(drag_client, Backward),
-        "M-C-bracketleft" => run_internal!(client_to_screen, &Selector::Index(0)),
-        "M-C-bracketright" => run_internal!(client_to_screen, &Selector::Index(1)),
-        "M-S-f" => run_internal!(toggle_client_fullscreen, &Selector::Focused),
-        "M-S-q" => run_internal!(kill_client),
-
+        "M-j" => run_internal!(cycle_client, Forward);
+        "M-k" => run_internal!(cycle_client, Backward);
+        "M-S-j" => run_internal!(drag_client, Forward);
+        "M-S-k" => run_internal!(drag_client, Backward);
+        "M-C-bracketleft" => run_internal!(client_to_screen, &Selector::Index(0));
+        "M-C-bracketright" => run_internal!(client_to_screen, &Selector::Index(1));
+        "M-S-f" => run_internal!(toggle_client_fullscreen, &Selector::Focused);
+        "M-S-q" => run_internal!(kill_client);
         // workspace management
-        "M-Tab" => run_internal!(toggle_workspace),
-        "M-A-period" => run_internal!(cycle_workspace, Forward),
-        "M-A-comma" => run_internal!(cycle_workspace, Backward),
-        "M-bracketright" => run_internal!(cycle_screen, Forward),
-        "M-bracketleft" => run_internal!(cycle_screen, Backward),
-        "M-S-bracketright" => run_internal!(drag_workspace, Forward),
-        "M-S-bracketleft" => run_internal!(drag_workspace, Backward),
+        "M-Tab" => run_internal!(toggle_workspace);
+        "M-A-period" => run_internal!(cycle_workspace, Forward);
+        "M-A-comma" => run_internal!(cycle_workspace, Backward);
+        "M-bracketright" => run_internal!(cycle_screen, Forward);
+        "M-bracketleft" => run_internal!(cycle_screen, Backward);
+        "M-S-bracketright" => run_internal!(drag_workspace, Forward);
+        "M-S-bracketleft" => run_internal!(drag_workspace, Backward);
 
         // Layout & window management
-        "M-grave" => run_internal!(cycle_layout, Forward),
-        "M-S-grave" => run_internal!(cycle_layout, Backward),
-        "M-A-Up" => run_internal!(update_max_main, More),
-        "M-A-Down" => run_internal!(update_max_main, Less),
-        "M-A-Right" => run_internal!(update_main_ratio, More),
-        "M-A-Left" => run_internal!(update_main_ratio, Less),
-        "M-A-C-Escape" => run_internal!(exit),
+        "M-grave" => run_internal!(cycle_layout, Forward);
+        "M-S-grave" => run_internal!(cycle_layout, Backward);
+        "M-A-Up" => run_internal!(update_max_main, More);
+        "M-A-Down" => run_internal!(update_max_main, Less);
+        "M-A-Right" => run_internal!(update_main_ratio, More);
+        "M-A-Left" => run_internal!(update_main_ratio, Less);
+        "M-A-C-Escape" => run_internal!(exit);
         "M-A-Escape" => power_menu;
 
-        forall_workspaces: config.workspaces => {
-            "M-{}" => focus_workspace,
-            "M-S-{}" => client_to_workspace,
-        }
+        refmap [ config.ws_range() ] in {
+            "A-C-{}" => focus_workspace [ index_selectors(config.workspaces.len()) ];
+            "A-C-S-{}" => client_to_workspace [ index_selectors(config.workspaces.len()) ];
+        };
     };
 
     let mouse_bindings = gen_mousebindings! {
