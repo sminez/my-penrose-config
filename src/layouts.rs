80aef4c4bb4b8ff1c11ad9e59d8cf83169dc664a
Innes Anderson-Morrison <innes.andersonmorrison@gmail.com>
2023-07-04 08:58:58 +0100

    adding more layouts

--- src/layouts.rs
+++ src/layouts.rs
@@ -1,15 +1,18 @@
 use crate::{MAX_MAIN, RATIO, RATIO_STEP};
 use penrose::{
-    builtin::layout::{MainAndStack, Monocle},
+    builtin::layout::{CenteredMain, Grid, MainAndStack, Monocle},
     core::layout::LayoutStack,
+    extensions::layout::Tatami,
     stack,
 };
 
 pub fn layouts() -> LayoutStack {
     stack!(
         MainAndStack::side(MAX_MAIN, RATIO, RATIO_STEP),
-        MainAndStack::side_mirrored(MAX_MAIN, RATIO, RATIO_STEP),
         MainAndStack::bottom(MAX_MAIN, RATIO, RATIO_STEP),
+        CenteredMain::vertical(MAX_MAIN, RATIO, RATIO_STEP),
+        Tatami::boxed(RATIO, RATIO_STEP),
+        Grid::boxed(),
         Monocle::boxed()
     )
 }
